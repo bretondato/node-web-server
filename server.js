ed58b2158d7e71fe409ba978ff3d30534c45c898
bretondato <brenno.tondato@gmail.com>
2018-03-10 22:17:43 -0300

    Inital commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const hbs = require('hbs');
+const path = require('path');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use(function (req, res, next) {
+    var now = new Date().toString();
+    var log = now + ' ' + req.method + ' ' + req.url + ' ' + req.ip + '\n';
+
+    fs.appendFile('serverLog.log', log, function (err) {
+        if(err){
+            console.log('Unable to append to serverLog file');
+        }
+    });
+
+    next();
+});
+
+hbs.registerHelper('getCurrentYear', function () {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', function (text) {
+   return text.toUpperCase();
+});
+
+
+app.get('/', function (req, res) {
+    res.render('home.hbs', {
+        pageTitle: 'About Page',
+        welcomeMessage:'Ola Bem Vindo ao Bretondato Site'
+    });
+});
+
+app.get('/about', function (req, res) {
+   res.render('about.hbs', {
+       pageTitle: 'About Page'
+   });
+});
+
+app.get('/bad', function (req, res) {
+    res.send({
+        errorMessage:"Unable to handle request"
+    });
+});
+
+app.listen(3000, function () {
+    console.log('App listening at port 3000');
+});
